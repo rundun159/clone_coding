8cac6b35c886a63c7b67bd100608fe0affe514d8
TH <xogud1231@gmail.com>
2022-04-14 15:47:52 +0900

    addEventListener

--- portfolio/main.js
+++ portfolio/main.js
@@ -2,18 +2,8 @@
 
 const navbar = document.querySelector('#navbar');
 const navbarHeight= navbar.getBoundingClientRect().height;
-const home = document.querySelector('#home');
-const homeTop = home.getBoundingClientRect().top;
-const about = document.querySelector('#about');
-const aboutTop = about.getBoundingClientRect().top;
-const skills = document.querySelector('#skills');
-const skillsTop = skills.getBoundingClientRect().top;
-const work = document.querySelector('#work');
-const workTop = work.getBoundingClientRect().top;
-const testimonials = document.querySelector('#testimonials');
-const testimonialsTop = testimonials.getBoundingClientRect().top;
-const contact = document.querySelector('#contact');
-const contactTop = contact.getBoundingClientRect().top;
+const contact_btn = document.querySelector('#home__contact');
+
 
 document.addEventListener('scroll', ()=>{
   if (window.scrollY > navbarHeight) {
@@ -24,51 +14,19 @@ document.addEventListener('scroll', ()=>{
   };
 });
 
-function moveHome(){
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
-}
+navbar.addEventListener('click', (event)=>{
+  const target = event.target;
+  const link = target.dataset.link;
+  if(!link)
+    return;
+  customScrollIntoView(link);
+})
 
-function moveAbout(){
-  window.scrollTo({
-    top: aboutTop,
-    left: 0,
-    behavior: 'smooth'
-  });
-}
+contact_btn.addEventListener('click',(event) => {
+  customScrollIntoView('#contact');
+});
 
-function moveSkills(){
-  window.scrollTo({
-    top: skillsTop,
-    left: 0,
-    behavior: 'smooth'
-  });
-}
 
-function moveMyWork(){
-  window.scrollTo({
-    top: workTop,
-    left: 0,
-    behavior: 'smooth'
-  });
+function customScrollIntoView(selector){
+  document.querySelector(selector).scrollIntoView({behavior: "smooth"});
 }
-
-function moveTestimonial(){
-  window.scrollTo({
-    top: testimonialsTop,
-    left: 0,
-    behavior: 'smooth'
-  });
-}
-
-function moveContact(){
-  window.scrollTo({
-    top: contactTop,
-    left: 0,
-    behavior: 'smooth'
-  });
-}
-
