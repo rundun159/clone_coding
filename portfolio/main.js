2120b6863a933963e2fbcb00550048a12eaba54c
TH <xogud1231@gmail.com>
2022-04-14 16:12:30 +0900

    scroll fadeout implemented

--- portfolio/main.js
+++ portfolio/main.js
@@ -3,15 +3,20 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight= navbar.getBoundingClientRect().height;
 const contact_btn = document.querySelector('#home__contact');
-
+const home__container = document.querySelector('#home__container');
+const aboutTop = document.querySelector('#about').getBoundingClientRect().top;
 
 document.addEventListener('scroll', ()=>{
+  let home_op = 1;
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   }
   else{
     navbar.classList.remove('navbar--dark');
+    return;
   };
+  home_op = 1.0 * (aboutTop-window.scrollY) / parseFloat(aboutTop);
+  home__container.style.opacity = home_op;  
 });
 
 navbar.addEventListener('click', (event)=>{
